b06a84dc4449be57e92b8d5f3d9d71f15938a6b1
Alaa <Alaa_m_y_2002@hotmail.com>
2024-02-06 03:56:40 +0300

    first version of plog by using flutter

---
+++ hw3_alaa_alyahya/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:hw3_alaa_alyahya/home_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: const HomeScreen(),
+    );    
+  }
+}
